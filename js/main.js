994c29b09889a2c679a717c926c27e5d3a6bc8e6
WortelNgoding <wortelngoding@gmail.com>
2023-12-12 16:20:28 +0700

    feat : init project

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+                
+            }
+        });
+    });
+
+    $('.accordion-heading').click(function(){
+
+        $('.accordion .accordion-content').slideUp();
+
+        $(this).next('.accordion-content').slideDown();
+
+    })
+
+});
+
+/** SEARCH SECTION START **/
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    }
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
---
+++ koneksi.php
@@ -0,0 +1,4 @@
+<?php 
+//cara koneksi kedalam database 
+$koneksi = mysqli_connect('localhost', 'root', '', 'db_ecommerce_bunga');
+?>
