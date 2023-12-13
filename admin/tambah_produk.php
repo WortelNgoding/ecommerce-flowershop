ab1b18bee68693ea96ab89e25c4e17a92dd1467e
WortelNgoding <wortelngoding@gmail.com>
2023-12-13 10:42:03 +0700

    feat(admin) : add products to database

--- admin/tambah_produk.php
+++ admin/tambah_produk.php
@@ -1,3 +1,24 @@
+<?php
+require '../functions.php';
+
+if (isset($_POST['submit'])) {
+
+    //cek apakah data berhasil ditambahkan atau tidak 
+    if (tambahProduk($_POST) > 0) {
+        echo
+        "<script>
+          alert('Data berhasil ditambahkan!');
+          document.location.href = 'tambah_produk.php';
+      </script>";
+    } else {
+        echo
+        "<script>
+          alert('Data gagal ditambahkan!');
+      </script>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -5,6 +26,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/tambah_produk.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <title>Tambah Produk</title>
 </head>
 
 <body>
@@ -12,17 +34,16 @@
     <div class="btn-back">
         <a href="index.php"><i class="fas fa-arrow-left"></i></a>
     </div>
-
     <h2>Tambah Produk</h2>
 
     <div class="container">
-        <form action="">
+        <form action="" method="post" enctype="multipart/form-data">
             <div class="row">
                 <div class="col-25">
                     <label for="nama"> Nama</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="nama" name="nama" placeholder="Nama Produk...">
+                    <input type="text" id="nama" name="nama_produk" placeholder="Nama Produk...">
                 </div>
             </div>
             <div class="row">
@@ -30,15 +51,7 @@
                     <label for="harga">Harga</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="harga" name="harga" placeholder="Harga Produk...">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-25">
-                    <label for="formFileDisabled" class="form-label">Foto Produk</label>
-                </div>
-                <div class="mb-3">
-                    <input class="form-control" type="file">
+                    <input type="text" id="harga" name="harga_produk" placeholder="Harga Produk...">
                 </div>
             </div>
             <div class="row">
@@ -46,11 +59,19 @@
                     <label for="subject">Deskripsi</label>
                 </div>
                 <div class="col-75">
-                    <textarea id="deskripsi" name="Deskripsi" placeholder="Tulisan Deskripsi Produkmu..." style="height:200px; resize:none;"></textarea>
+                    <textarea id="subject" name="deksripsi_produk" placeholder="Tulisan Deskripsi Produkmu..." style="height:200px; resize:none;"></textarea>
                 </div>
             </div>
             <div class="row">
-                <input type="submit" value="Submit">
+                <div class="col-25">
+                    <label for="formFileDisabled" class="form-label">Foto Produk</label>
+                </div>
+                <div class="mb-3">
+                    <input class="form-control" type="file" name="gambar_produk">
+                </div>
+            </div>
+            <div class="row">
+                <input type="submit" name="submit">
             </div>
         </form>
     </div>
--- functions.php
+++ functions.php
@@ -15,3 +15,73 @@ function tambahUser($data)
 
     return mysqli_affected_rows($koneksi);
 }
+
+function tambahProduk($data)
+{
+    global $koneksi;
+
+    $nama_produk = htmlspecialchars($data["nama_produk"]);
+    $harga_produk = htmlspecialchars($data["harga_produk"]);
+    $deskripsi_produk = htmlspecialchars($data["deksripsi_produk"]);
+
+
+    $gambar = upload();
+    if (!$gambar) {
+        return false;
+    }
+
+
+    $query = "INSERT INTO tb_produk VALUES ('', '$nama_produk', '$harga_produk', '$deskripsi_produk', '$gambar')";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+function upload()
+{
+    $namaFile = $_FILES['gambar_produk']['name'];
+    $ukuranFile = $_FILES['gambar_produk']['size'];
+    $error = $_FILES['gambar_produk']['error'];
+    $tmpName = $_FILES['gambar_produk']['tmp_name'];
+
+    //cek apakah tidak ada gambar yang diupload
+    if ($error === 4) {
+        echo  "<script>
+        alert('Pilih gambar terlebih dahulu!');
+            </script>";
+        return false;
+    }
+
+
+
+    //cek apakah yang diupload gambar atau bukan
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+    $ekstensiGambar = explode('.', $namaFile); // -> Contoh Gambar.jpg = ['Gambar', 'jpg'];
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        echo  "<script>
+        alert('Tolong upload berupa gambar!');
+            </script>";
+        return false;
+    }
+
+    //cek jika ukuran terlalu besar 
+    if ($ukuranFile > 1000000) {
+        echo  "<script>
+        alert('Ukuran gambar terlalu besar!');
+            </script>";
+        return false;
+    }
+
+    // gambar siap diupload
+    //generate nama gambar baru 
+    $namaFileBaru = uniqid();
+    $namaFileBaru .=  '.';
+    $namaFileBaru .= $ekstensiGambar;
+
+
+    move_uploaded_file($tmpName, '../product_images/' . $namaFileBaru);
+
+    return $namaFileBaru;
+}
